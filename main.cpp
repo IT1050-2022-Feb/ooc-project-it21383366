e8583fb608da912ca8ae1a0edbed00b63482fdeb
IT21383366 <99157125+it21383366@users.noreply.github.com>
2022-05-18 19:07:33 +0000

    Updated.

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "sdihfbuiysfgs" << endl;
+
+  return 0;
+}
---
+++ main.sh
@@ -0,0 +1,7 @@
+echo "# ooc-project-it21383366" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/IT1050-2022-Feb/ooc-project-it21383366.git
+git push -u origin main
