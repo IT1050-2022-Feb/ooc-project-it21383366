eeb97a2e44a840788b11306da63ff8c61648d31e
IT21385582 <99157519+IT21385582@users.noreply.github.com>
2022-05-19 10:03:09 +0000

    abc

---
+++ shakila.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+ 	int no1,no2;
+ 	int sum;
+ 	
+ 	cout<<"input 1 and 2  : ";
+ 	cin>>no1>>no2;
+ 	sum=no1+no2;
+ 	cout<<"sum is"<<sum<<endl;
+ 	return 0;
